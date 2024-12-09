041633668d50a085a035f76c50f734057efae8c2
Frengky04 <221712051@students.uajy.ac.id>
2024-12-09 18:10:50 +0700

    first commit

--- app/api/pelanggan/route.ts
+++ app/api/pelanggan/route.ts
@@ -1,13 +1,13 @@
-import { NextResponse } from 'next/server';
-import { sql } from '@vercel/postgres'; // Gunakan library yang sesuai untuk database Anda
+// import { NextResponse } from 'next/server';
+// import { sql } from '@vercel/postgres'; // Gunakan library yang sesuai untuk database Anda
 
-export async function GET() {
-  try {
-    const result = await sql`SELECT nomor_hp_pelanggan FROM pelanggan;`; // Query database
-    const phoneNumbers = result.rows.map((row) => row.nomor_hp_pelanggan);
-    return NextResponse.json(phoneNumbers);
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    return NextResponse.json({ error: 'Failed to fetch data' }, { status: 500 });
-  }
-}
+// export async function GET() {
+//   try {
+//     const result = await sql`SELECT nomor_hp_pelanggan FROM pelanggan;`; // Query database
+//     const phoneNumbers = result.rows.map((row) => row.nomor_hp_pelanggan);
+//     return NextResponse.json(phoneNumbers);
+//   } catch (error) {
+//     console.error('Error fetching data:', error);
+//     return NextResponse.json({ error: 'Failed to fetch data' }, { status: 500 });
+//   }
+// }
